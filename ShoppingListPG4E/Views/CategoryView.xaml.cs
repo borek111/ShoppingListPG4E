6623c2f47182fb3552c0706c3b1ace3725e21abf
Przemysław Grabecki <przemekgrabecki@gmail.com>
2025-11-27 10:12:10 +0100

    poprawki dla widokow

---
+++ ShoppingListPG4E/Views/CategoryView.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace ShoppingListPG4E.Views
+{
+    public partial class CategoryView : ContentView
+    {
+        public CategoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
